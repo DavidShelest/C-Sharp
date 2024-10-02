d3366d43dfedd42b72ea8a5a449e3964bbdf9ac1
DavidShelest <david.rustle47@gmail.com>
2024-10-02 20:12:33 +1000

    Remainder

--- remainder/Program.cs
+++ remainder/Program.cs
@@ -10,10 +10,9 @@
             Console.WriteLine("Введите делитель (целое число): ");
             int divisor = Convert.ToInt32(Console.ReadLine());
 
-            int quotient = dividend / divisor;
-            int remainder = dividend % divisor;
-
             if (divisor != 0)
+                int quotient = dividend / divisor;
+                int remainder = dividend % divisor;
                 Console.WriteLine($"{dividend}/{divisor} = {quotient} остаток {remainder}");
             else
                 Console.WriteLine("Ошибка! Деление на ноль!");
