f1f7a5c127f13ca638408444d6fb8ca4ffe90c4b
DavidShelest <david.rustle47@gmail.com>
2024-10-06 21:19:02 +1000

    Copying array

---
+++ copying array/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace copying_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[10]{7, 4, 3, 5, 3, 2, 2, 9, 5, 12};
+
+            Console.WriteLine("Элементы массива Numbers: ");
+            foreach (int i in Numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            int[] Numbers2 = new int[10];
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers2[i] = Numbers[i];
+            }
+
+            Console.WriteLine("Элементы массива Numbers2: ");
+
+            foreach (int i in Numbers2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
