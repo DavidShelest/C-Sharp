5bceeb2af25e82e9a6b6ce06e4ac39fcc7efc754
DavidShelest <david.rustle47@gmail.com>
2024-10-08 18:32:04 +1000

    666

---
+++ months of the year/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace months_of_the_year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] AllowedNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+            Console.WriteLine("Введите число, соответствующее номеру месяца: ");
+            int result = Convert.ToInt32(Console.ReadLine());
+        }
+    
+            //if AllowedNumbers
+
+       
+    }
+
+    enum MonthsOfTheYear
+    {
+        January = 1,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+}
