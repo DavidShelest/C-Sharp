9f8a984aae47b84e224922dc7150f802015cdf3e
DavidShelest <david.rustle47@gmail.com>
2024-10-12 14:19:59 +1000

    MonthsOfTheYear

---
+++ months of the year/MonthsOfTheYear.cs
@@ -0,0 +1,21 @@
+﻿namespace months_of_the_year;
+
+internal partial class Program
+{
+    enum MonthsOfTheYear
+    {
+        January = 1,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+}
--- months of the year/Program.cs
+++ months of the year/Program.cs
@@ -1,34 +1,28 @@
 ﻿namespace months_of_the_year
 {
-    internal class Program
+    internal partial class Program
     {
+
         static void Main(string[] args)
         {
 
             int[] AllowedNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 
             Console.WriteLine("Введите число, соответствующее номеру месяца: ");
-            int result = Convert.ToInt32(Console.ReadLine());
+            int num;
+
+            bool result = Int32.TryParse(Console.ReadLine(), out num);
+
+            if (result && AllowedNumbers.Contains(num))
+            {
+                Console.WriteLine($"Выбран месяц: {Enum.GetName(typeof(MonthsOfTheYear), num)}");
+            }
+            else
+            {
+                Console.WriteLine("Необходимо ввести число от 1 до 12 (соответствующее номеру месяца)!");
+            }
         }
-    
-            //if AllowedNumbers
 
-       
     }
 
-    enum MonthsOfTheYear
-    {
-        January = 1,
-        February,
-        March,
-        April,
-        May,
-        June,
-        July,
-        August,
-        September,
-        October,
-        November,
-        December
-    }
 }
